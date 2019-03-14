8729d1f4c2a208481cacfe03e7e5ad3d5a78fe40
Andrey Bogdanov <bogdanov6391@gmail.com>
2019-03-14 11:00:45 +0300

    add payment methods

---
+++ database/migrations/2019_03_14_055742_create_payment_methods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+
+        \DB::insert(\Storage::disk('sql')->get('payment_methods.sql'));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+}
